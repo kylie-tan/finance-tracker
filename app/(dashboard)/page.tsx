870a31db011d5a3d934de15fa0463eff8375bbbe
Kylie <hello.kylieeeee@gmail.com>
2025-07-31 15:23:27 +0800

    Backup commit: before adding Clerkprovider to app

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-		<Button>
-			Clicke Me
-		</Button>
+		<p>
+			This is an authenticated route
+		</p>
 	)
 }
