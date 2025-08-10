9fb6309464e24322d31485f2ef4ba22c4615edda
Kylie <hello.kylieeeee@gmail.com>
2025-08-10 14:43:37 +0800

    fix: drizzle.config.ts
    	saving copy to updating rest of migration scripts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,35 +4,11 @@ import { defineConfig } from "drizzle-kit";
 config({ path: ".env.local" });
 
 export default defineConfig({
-  dialect: "postgresql",
   schema: "./db/schema.ts",
-
-  dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
-  },
-});
-
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
   dialect: "postgresql",
-  schema: "./src/schema.ts",
-  out: "./drizzle",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
 });
-
-
-// import { config } from "dotenv";
-// import { defineConfig } from "drizzle-kit";
-
-
-// config({ path: ".env.local" });
-
-// export default defineConfig({
-// 	schema: "./db/schema.ts",
-// 	driver: "pg",
-// 	dbCredentials: {
-// 		connectionString: process.env.DATABASEURL!,
-// 	},
-// 	verbose: true,
-// 	strict: true,
-// });
