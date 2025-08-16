8a37db6036f1d21138900bd975e3411dab41bc74
Kylie <hello.kylieeeee@gmail.com>
2025-08-17 00:55:41 +0800

    add: accounts table

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,27 +1,60 @@
+"use client"
+
+import { Plus } from "lucide-react";
+
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+
 import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/data-table";
 import {
 	Card,
 	CardContent,
 	CardHeader,
 	CardTitle
-} from "@/components/ui/card"
-import { Plus } from "lucide-react";
+} from "@/components/ui/card";
+
+import { columns, Payment } from "./columns";
+
+
+const data: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+	{
+    id: "459cd52f",
+    amount: 50,
+    status: "success",
+    email: "a@example.com",
+  },
+];
 
 const AccountsPage = () => {
-
+	const newAccount = useNewAccount();
 
 	return (
-		<div>
+		<div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
 			<Card className="border-none drop-shadow-sm">
 				<CardHeader className="flex flex-col gap-y-2 lg:flex-row lg:items-center lg: justify-between">
 					<CardTitle className="text-xl line-clamp-1">
 						Accounts page
 					</CardTitle>
-					<Button className="w-full lg:w-auto" size="sm">
+					<Button onClick={newAccount.onOpen} className="w-full lg:w-auto" size="sm">
 						<Plus className="size-4 mr-2" />
 						Add new
 					</Button>
 				</CardHeader>
+				<CardContent>
+					<DataTable
+						filterKey="email"
+						columns={columns}
+						data={data}
+						onDelete={() => {}}
+						disabled={false}
+					/>
+				</CardContent>
 			</Card>
 		</div>
 	);
