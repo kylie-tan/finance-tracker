3b5c201e827007ebe2a5bedbe9a62395eb1897b8
Kylie <hello.kylieeeee@gmail.com>
2025-08-25 23:49:56 +0800

    add: csv upload form and table
    fix: button mobile view styling in csv table

--- app/(dashboard)/transactions/import-card.tsx
+++ app/(dashboard)/transactions/import-card.tsx
@@ -39,6 +39,30 @@ export const ImportCard = ({
 	const headers = data[0];
 	const body = data.slice(1);
 
+	const onTableHeadSelectChange = (
+		columnIndex: number,
+		value: string | null
+	) => {
+		setSelectedColumns((prev) => {
+			const newSelectedColumns = {...prev};
+
+			for (const key in newSelectedColumns) {
+				if (newSelectedColumns[key] === value) {
+					newSelectedColumns[key] = null;
+				}
+			}
+
+			if (value === "skip") {
+				value = null;
+			}
+
+			newSelectedColumns[`column_${columnIndex}`] = value;
+			return newSelectedColumns;
+		});
+	};
+
+	const progress = Object.values(selectedColumns).filter(Boolean).length;
+
 	return (
 		<div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
 			<Card className="border-none drop-shadow-sm">
@@ -46,9 +70,21 @@ export const ImportCard = ({
 					<CardTitle className="text-xl line-clamp-1">
 						Import Transaction
 					</CardTitle>
-					<div className="flex items-center gap-x-2">
-						<Button onClick={onCancel} size="sm">
+					<div className="flex flex-col lg:flex-row gap-y-2 items-center gap-x-2 w-full lg:w-auto">
+						<Button
+							onClick={onCancel}
+							size="sm"
+							className="w-full lg:w-auto"
+						>
 							Cancel
+						</Button>
+						<Button
+							size="sm"
+							disabled={progress < requiredOptions.length}
+							onClick={() => {}}
+							className="w-full lg:w-auto"
+						>
+							Continue ({progress} / {requiredOptions.length})
 						</Button>
 					</div>
 				</CardHeader>
@@ -57,7 +93,7 @@ export const ImportCard = ({
 						headers={headers}
 						body={body}
 						selectedColumns={selectedColumns}
-						onTableHeadSelectChange={() => {}}
+						onTableHeadSelectChange={onTableHeadSelectChange}
 					/>
 				</CardContent>
 			</Card>
--- app/(dashboard)/transactions/table-head-select.tsx
+++ app/(dashboard)/transactions/table-head-select.tsx
@@ -1,3 +1,12 @@
+import { cn } from "@/lib/utils";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+
 type Props = {
 	columnIndex: number;
 	selectedColumns: Record<string, string | null>;
@@ -11,7 +20,6 @@ const options = [
 	"amount",
 	"date",
 	"payee",
-	"notes",
 ];
 
 export const TableHeadSelect = ({
@@ -19,9 +27,39 @@ export const TableHeadSelect = ({
 	selectedColumns,
 	onChange,
 }: Props) => {
+	const currentSelection = selectedColumns[`column_${columnIndex}`];
+
 	return (
-		<div>
-			Select
-		</div>
+		<Select
+			value={currentSelection || ""}
+			onValueChange={(value) => onChange(columnIndex, value)}
+		>
+			<SelectTrigger
+				className={cn(
+					"focus:ring-offset-0 focus:ring-transparent outline-none border-none bg-transparent capitalize",
+					currentSelection && "text-blue-500",
+				)}
+			>
+				<SelectValue placeholder="Skip" />
+			</SelectTrigger>
+			<SelectContent>
+				<SelectItem value="skip">Skip</SelectItem>
+				{options.map((option, index) => {
+					const disabled =
+						Object.values(selectedColumns).includes(option) && selectedColumns[`column_${columnIndex}`] !== option;
+
+					return (
+					<SelectItem
+						key={index}
+						value={option}
+						disabled={disabled}
+						className="capitalize"
+					>
+						{option}
+					</SelectItem>
+					)
+				})}
+			</SelectContent>
+		</Select>
 	);
 };
